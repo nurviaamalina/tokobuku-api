358b042baa78e29d2c22b80a68d6a42b05d782c1
nurviaamalina <nurviamalina@gmail.com>
2024-10-21 06:09:59 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+    
+     Route::get('/bukus/kategori/{kategori_id}', [BukuController::class, 'getByKategori']);
 });
